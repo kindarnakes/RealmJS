b588b8a764ddc41c2ca11bc7ca623d16f999fbd6
Santos Castillo <castilloperezsantos@gmail.com>
2021-01-19 19:17:03 +0100

    Factura get

--- factura.js
+++ factura.js
@@ -24,14 +24,56 @@ const updateFactura = (request, response) =>{
 
 }
 const getFacturas = (request, response) =>{
+    
+    try {
+        let factura = realm.objects('Factura');
+        
+        if(factura){
+            response.status(200).json(factura);
+          }else{
+            console.log('No hay facturas');
+        }
+    } catch (error) {
+        
+    }
+    
+    
+   
+    
 
 }
 const getFactura = (request, response) =>{
+    try {
+        let factura = realm.objects('Factura');
+        let idFactura = factura.filtered('id = "id"');
+        
+        if(factura){
+            response.status(200).json(idfactura);
+          }else{
+            response.status(403).json("No existe esa factura con ese ID");
+            return;
+        }
+    } catch (error) {
+        
+    }
 
 
 }
 const removeFactura = (request, response) =>{
 
+    try {
+        let factura = realm.objects('Factura');
+        let idFactura = factura.filtered('id = "id"');
+        
+        if(factura){
+            realm.delete(idFactura);
+          }else{
+            response.status(403).json("No existe esa factura con ese ID");
+            return;
+        }
+    } catch (error) {
+        
+    }
 }
 
 
