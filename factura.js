8480dee09f7bab6b1387424d257f3f5a1e14d1a2
Agustin <agustinjimenezpolonio@gmail.com>
2021-01-20 15:15:17 +0100

    RemoveFactura hecho

--- factura.js
+++ factura.js
@@ -1,12 +1,12 @@
 let realm = require("realm");
-let clienteModel =  require('./cliente');
+let clienteModel = require('./cliente');
 
 const FacturaSchema = {
     name: 'Factura',
     primaryKey: 'id',
-    properties:{
+    properties: {
         id: 'string',
-        importe: {type: 'float', default: 0.0},
+        importe: { type: 'float', default: 0.0 },
         fecha: 'date',
         cliente_dni: 'string'
         //cliente: {type: 'linkingObjects', objectType: 'Cliente', property: 'facturas'}
@@ -18,140 +18,149 @@ const FacturaSchema = {
 
 
 
-const createFactura = (request, response) =>{
+const createFactura = (request, response) => {
     let name = '';
-    let id='';
-    let importe='';
+    let id = '';
+    let importe = '';
     let fecha;
-    let cliente_dni='';
+    let cliente_dni = '';
     console.log(request.body);
     if (request.body) {
         name = request.body.name;
-        id=request.body.id;
+        id = request.body.id;
         importe = request.body.importe;
         fecha = request.body.fecha;
-        cliente_dni=request.body.cliente_dni;
-       
-        try{
-        realm.open({ schema: [FacturaSchema, clienteModel.ClienteSchema] }).then(realm => {
-            try {
-            realm.write(() => {
-               
-                
-                realm.create('Factura', {
-              
-                    id: id, importe:importe, fecha:fecha, cliente_dni:cliente_dni 
-                  
-                  });
-                  response.status(200).json('done');
+        cliente_dni = request.body.cliente_dni;
+
+        try {
+            realm.open({ schema: [FacturaSchema, clienteModel.ClienteSchema] }).then(realm => {
+                try {
+                    realm.write(() => {
+
+
+                        realm.create('Factura', {
+
+                            id: id, importe: importe, fecha: fecha, cliente_dni: cliente_dni
+
+                        });
+                        response.status(200).json('done');
+                    });
+                } catch (err) {
+                    console.log(err);
+                    response.status(409).json('Not done, key repeat');
+                }
             });
-        } catch (err) {
-            console.log(err);
-            response.status(409).json('Not done, key repeat');
-          }
-         });
-        }catch (e) {
+        } catch (e) {
             response.status(409).json('Not done');
             console.log("Error on creation");
         }
-        }
+    }
 }
 
-const updateFactura = (request, response) =>{
+const updateFactura = (request, response) => {
     let name = '';
-    let id='';
-    let importe='';
+    let id = '';
+    let importe = '';
     let fecha;
-    let cliente_dni='';
+    let cliente_dni = '';
     console.log(request.body);
     if (request.body) {
         name = request.body.name;
-        id=request.body.id;
+        id = request.body.id;
         importe = request.body.importe;
         fecha = request.body.fecha;
-        cliente_dni=request.body.cliente_dni;
-       
-        try{
-        realm.open({ schema: [FacturaSchema, clienteModel.ClienteSchema] }).then(realm => {
-            try {
-            realm.write(() => {
-               
-                
-                realm.create('Factura', {
-              
-                    id: id, importe:importe, fecha:fecha, cliente_dni:cliente_dni 
-                  
-                  }, 'modified' );
-                  response.status(200).json('done');
+        cliente_dni = request.body.cliente_dni;
+
+        try {
+            realm.open({ schema: [FacturaSchema, clienteModel.ClienteSchema] }).then(realm => {
+                try {
+                    realm.write(() => {
+
+
+                        realm.create('Factura', {
+
+                            id: id, importe: importe, fecha: fecha, cliente_dni: cliente_dni
+
+                        }, 'modified');
+                        response.status(200).json('done');
+                    });
+                } catch (err) {
+                    console.log(err);
+                    response.status(409).json('Not done, key repeat');
+                }
             });
-        } catch (err) {
-            console.log(err);
-            response.status(409).json('Not done, key repeat');
-          }
-         });
-        }catch (e) {
+        } catch (e) {
             response.status(409).json('Not done');
             console.log("Error on creation");
         }
-        }
+    }
 }
-const getFacturas = (request, response) =>{
-    
+const getFacturas = (request, response) => {
+
     try {
         realm.open({ schema: [FacturaSchema, clienteModel.ClienteSchema] }).then(realm => {
-        let factura = realm.objects('Factura');
-        
-        if(factura){
-            response.status(200).json(factura);
-          }else{
-            console.log('No hay facturas');
-        }
-    })
-} catch (error) {
-    response.status(403).json('No hay facturas');
-        console.log('No hay facturas');
-        
-    }
-   
+            let factura = realm.objects('Factura');
 
-}
-
-
-const getFactura = (request, response) =>{
-    try {
-        realm.open({ schema: [FacturaSchema, clienteModel.ClienteSchema] }).then(realm => {
-        let factura = realm.objects('Factura');
-        let idFactura = factura.filtered('id = $0', request.params.id);
-        
-        if(idFactura){
-            response.status(200).json(idFactura);
-          }else{
-            response.status(403).json("No existe esa factura con ese ID");
-            return;
-        }
-    })
-} catch (error) {
-    response.status(403).json('No hay facturas');
-        console.log('No hay facturas');
-        
-    }
-
-
-}
-const removeFactura = (request, response) =>{
-
-    try {
-        let factura = realm.objects('Factura');
-        let idFactura = factura.filtered('id = "id"');
-        
-        if(factura){
-            realm.delete(idFactura);
-          }else{
-            response.status(403).json("No existe esa factura con ese ID");
-            return;
-        }
+            if (factura) {
+                response.status(200).json(factura);
+            } else {
+                console.log('No hay facturas');
+            }
+        })
     } catch (error) {
-        
+        response.status(403).json('No hay facturas');
+        console.log('No hay facturas');
+
+    }
+
+
+}
+
+
+const getFactura = (request, response) => {
+    try {
+        realm.open({ schema: [FacturaSchema, clienteModel.ClienteSchema] }).then(realm => {
+            let factura = realm.objects('Factura');
+            let idFactura = factura.filtered('id = $0', request.params.id);
+
+            if (idFactura) {
+                response.status(200).json(idFactura);
+            } else {
+                response.status(403).json("No existe esa factura con ese ID");
+                return;
+            }
+        })
+    } catch (error) {
+        response.status(403).json('No hay facturas');
+        console.log('No hay facturas');
+
+    }
+
+
+}
+const removeFactura = (request, response) => {
+
+    try {
+        realm.open({ schema: [FacturaSchema, clienteModel.ClienteSchema] }).then(realm => {
+            console.log(request.params.id);
+            let factura = realm.objects('Factura').filtered('id = $0', request.params.id);
+            if (factura) {
+                realm.write(() => {
+                    try {
+                        realm.delete(factura);
+                        response.status(200).json('removed');
+                    } catch (err) {
+                        console.log(err);
+                        response.status(403).json('Not removed');
+                    }
+                });
+            } else {
+                response.status(403).json('Not removed');
+            }
+        })
+    } catch (error) {
+        response.status(403).json('Not removed');
+        console.log(error)
     }
 }
 
